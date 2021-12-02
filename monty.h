8a2792a95f06b211c014b819e8e1a7ea5a58e669
AmanGr <alphagr110@gmail.com>
2021-12-02 07:18:38 -0800

    code

--- monty.h
+++ monty.h
@@ -1,80 +1,47 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
-#include <limits.h>
+#include <fcntl.h>
 #include <ctype.h>
 
-#define UNUSED(x) (void)(x)
-#define TRUE 1
-#define FALSE 0
-#define DELIMS "\n \t\r"
+#include "structs.h"
 
-/* Global */
+/* macros */
+#define DELIMITERS "\n\t\r "
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
+/* struct search function */
+int opcode_struct(char *opcode, stack_t **stack, unsigned int line_number);
 
+/* stack functions */
+void push(stack_t **stack, unsigned int line_number, char *n);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int n_line);
-} instruction_t;
+/* print functions */
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 
-/**
- * struct glob_s - globally useful variables, all rolled into one
- * @top: double pointer to top of stack
- * @ops: double pointer to an instruction struct
-**/
-typedef struct glob_s
-{
-	stack_t **top;
-	instruction_t **ops;
-} glob_t;
+/* calc functions */
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 
-extern glob_t glob;
-
-/* monty.c */
-void stack_init(stack_t **head);
-void free_all(void);
-
-int process_file(char *filename, stack_t **stack);
-void call_functions(stack_t **stack, char *op, unsigned int line_number);
-
-void instruction_push(stack_t **stack, unsigned int line_number);
-void instruction_pall(stack_t **stack, unsigned int line_number);
-
-int _strtol(char *num_string, unsigned int line_number);
+/* utility functions */
+size_t stack_length(stack_t **stack);
+void free_stack(stack_t **stack);
+void free_all(stack_t *stack, char *line, FILE *fp);
+void frees(stack_t *head);
 
 
-#endif
+#endif /* MONTY_H */
