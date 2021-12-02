e42eb7f0d2a8949a8a79b461bd453af5d2245e7f
AmanGr <alphagr110@gmail.com>
2021-12-02 07:13:44 -0800

    code

--- monty.h
+++ monty.h
@@ -1,15 +1,24 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <ctype.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
 
-/* data structures */
+#define UNUSED(x) (void)(x)
+#define TRUE 1
+#define FALSE 0
+#define DELIMS "\n \t\r"
+
+/* Global */
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -18,7 +27,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -27,43 +36,45 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
+
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int n_line);
 } instruction_t;
 
-/* opcodes related prototypes */
-void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number);
-void push(stack_t **h, unsigned int line_number, const char *n);
-void pop(stack_t **h, unsigned int line_number);
-void pint(stack_t **h, unsigned int line_number);
-void pall(stack_t **h, unsigned int line_number);
-void swap(stack_t **h, unsigned int line_number);
-void nop(stack_t **h, unsigned int line_number);
-void pchar(stack_t **h, unsigned int line_number);
-void pstr(stack_t **h, unsigned int line_number);
-void rotl(stack_t **h, unsigned int line_number);
-void rotr(stack_t **h, unsigned int line_number);
+/**
+ * struct glob_s - globally useful variables, all rolled into one
+ * @top: double pointer to top of stack
+ * @ops: double pointer to an instruction struct
+**/
+typedef struct glob_s
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_t;
 
-/* calculator prototypes */
-void _add(stack_t **h, unsigned int line_number);
-void _sub(stack_t **h, unsigned int line_number);
-void _mul(stack_t **h, unsigned int line_number);
-void _div(stack_t **h, unsigned int line_number);
-void _mod(stack_t **h, unsigned int line_number);
+extern glob_t glob;
 
-/* doubly linked list related prototypes */
-int add_end_node(stack_t **h, int n);
-void delete_end_node(stack_t **h);
-void free_dlist(stack_t **h);
+/* monty.c */
+void stack_init(stack_t **head);
+void free_all(void);
+
+int process_file(char *filename, stack_t **stack);
+void call_functions(stack_t **stack, char *op, unsigned int line_number);
+
+void instruction_push(stack_t **stack, unsigned int line_number);
+void instruction_pall(stack_t **stack, unsigned int line_number);
+
+int _strtol(char *num_string, unsigned int line_number);
+
 
 #endif
